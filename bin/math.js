b5bda8c64be2bf909508ee297a72af5f77da29ba
blohhs <domikblohi@gmail.com>
2025-05-17 17:01:51 +0300

    создание калькулятора

---
+++ bin/math.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const division = (a, b) => a / b;
+
+export { sum, substract, multiply, division };
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
-// eslint.config.mjs
-import js from '@eslint/js'
-import globals from 'globals'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
 
 export default defineConfig([
-  stylistic.configs.recommended,
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-])
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+]);
